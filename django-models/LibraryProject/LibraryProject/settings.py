be8895879fd57ef3cc6c99ebf1744c5be760ddff
tam-rat <tamrathabtamu1@gmail.com>
2026-01-30 19:16:43 +0300

    1

--- django-models/LibraryProject/LibraryProject/settings.py
+++ django-models/LibraryProject/LibraryProject/settings.py
@@ -44,7 +44,13 @@ DATABASES = {
     }
 }
 INSTALLED_APPS = [
-    ...
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    
+    'bookshelf',
     'relationship_app',
 ]
-
