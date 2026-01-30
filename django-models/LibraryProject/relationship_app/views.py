9d72f6cdfff1bb21854faba6cbdbbabb17a622db
tam-rat <tamrathabtamu1@gmail.com>
2026-01-30 18:12:35 +0300

    1

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
-from django.views.generic import DetailView
+
 from .models import Library
 from .models import Book
-
+from django.views.generic.detail import DetailView
 def list_books(request):
     books = Book.objects.all()
     return render(request, "relationship_app/list_books.html", {"books": books})
